ff1ba7143a089d567fef6a03a55a59975f52bc7c
michael <1122>
2019-05-17 21:55:12 +0800

    完成分类页面的数据动态渲染,新增分类和点击编辑按钮,改变提交框的 内容

--- controller/categoriesContr.js
+++ controller/categoriesContr.js
@@ -1,11 +1,12 @@
 const categoriesdb = require('../model/categoriesdb.js')
+const url = require('url')
 
 module.exports = {
     //处理静态页面
     categories:(req,res)=>{
         res.render('categories',{})
     },
-    
+
     //渲染动态数据资源
     getAllData :(req,res)=> {
         let selSql = `SELECT*FROM categories`
@@ -25,6 +26,48 @@ module.exports = {
             })
             
         })
+    },
+
+    //处理新增分类数据
+    addSort:(req,res)=>{
+        let params=req.body
+        // console.log(params);
+        let addSql = `INSERT INTO categories (name,slug)VALUES ('${params.name}','${params.slug}')`
+        categoriesdb.query(addSql,(err,result)=>{
+            if (err) {
+                res.send({
+                    status:400,
+                    msg:'出错啦'
+                })
+               return
+            }
+            res.send({
+                status:200,
+                msg:'新增成功',
+            })
+        })
+        
+    },
+
+    //处理分类编辑内容
+    selSortById:(req,res)=>{
+        let id = req.query.id
+        // console.log(id);
+        let selSql = `SELECT*FROM categories WHERE id=${id}`
+        categoriesdb.query(selSql,(err,result)=>{
+            if (err) {
+                res.send({
+                    status:400,
+                    msg:'出错了'
+                })
+                return
+            }
+            res.send({
+                status:200,
+                data:result[0]
+            })
+        })
+        
     }
 
 }
--- router/categoriesRouter.js
+++ router/categoriesRouter.js
@@ -5,5 +5,7 @@ const router = express()
 //外置路由链式结构
 router.get('/categories',categoriesContr.categories)//加载静态页面
         .get('/getAllData',categoriesContr.getAllData)//处理动态渲染数据路由
+        .post('/addSort',categoriesContr.addSort)//处理新增分类数据
+        .get('/selSortById',categoriesContr.selSortById)//处理编辑分类内容
 //暴露接口
 module.exports = router
