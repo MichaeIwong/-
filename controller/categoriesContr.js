f8581e6819d77323452abbc6dcf96bab7757ebc5
michael <1122>
2019-05-18 13:29:35 +0800

    完成了分类页面的增删改查

--- controller/categoriesContr.js
+++ controller/categoriesContr.js
@@ -3,71 +3,139 @@ const url = require('url')
 
 module.exports = {
     //处理静态页面
-    categories:(req,res)=>{
-        res.render('categories',{})
+    categories: (req, res) => {
+        res.render('categories', {})
     },
 
     //渲染动态数据资源
-    getAllData :(req,res)=> {
+    getAllData: (req, res) => {
         let selSql = `SELECT*FROM categories`
-        categoriesdb.query(selSql,(err,result)=>{
+        categoriesdb.query(selSql, (err, result) => {
             // console.log(result);
-            if(err) {
+            if (err) {
                 res.send({
-                    status:400,
-                    msg:'出错了'
+                    status: 400,
+                    msg: '出错了'
                 })
                 return
             }
             res.send({
-                status:200,
-                msg:'加载完成',
-                data:result
+                status: 200,
+                msg: '加载完成',
+                data: result
             })
-            
+
         })
     },
 
     //处理新增分类数据
-    addSort:(req,res)=>{
-        let params=req.body
+    addSort: (req, res) => {
+        let params = req.body
         // console.log(params);
         let addSql = `INSERT INTO categories (name,slug)VALUES ('${params.name}','${params.slug}')`
-        categoriesdb.query(addSql,(err,result)=>{
+        categoriesdb.query(addSql, (err, result) => {
             if (err) {
                 res.send({
-                    status:400,
-                    msg:'出错啦'
-                })
-               return
-            }
-            res.send({
-                status:200,
-                msg:'新增成功',
-            })
-        })
-        
-    },
-
-    //处理分类编辑内容
-    selSortById:(req,res)=>{
-        let id = req.query.id
-        // console.log(id);
-        let selSql = `SELECT*FROM categories WHERE id=${id}`
-        categoriesdb.query(selSql,(err,result)=>{
-            if (err) {
-                res.send({
-                    status:400,
-                    msg:'出错了'
+                    status: 400,
+                    msg: '出错啦'
                 })
                 return
             }
             res.send({
-                status:200,
-                data:result[0]
+                status: 200,
+                msg: '新增成功',
             })
         })
+
+    },
+
+    //处理分类编辑内容
+    selSortById: (req, res) => {
+        let id = req.query.id
+        // console.log(id);
+        let selSql = `SELECT*FROM categories WHERE id=${id}`
+        categoriesdb.query(selSql, (err, result) => {
+            if (err) {
+                res.send({
+                    status: 400,
+                    msg: '出错了'
+                })
+                return
+            }
+            res.send({
+                status: 200,
+                data: result[0]
+            })
+        })
+
+    },
+
+    //处理编辑提交的数据
+    updateCategories: (req, res) => {
+        let params = req.body
+        // console.log(params);
+        let updSql = `UPDATE categories SET name='${params.name}',slug='${params.slug}' WHERE id = ${params.id}`
+        categoriesdb.query(updSql, (err, result) => {
+            if (err) {
+                res.send({
+                    status: 400,
+                    msg: '出错了'
+                })
+                return
+            }
+            res.send({
+                status: 200,
+                msg: '修改成功'
+            })
+        })
+
+    },
+
+    //处理删除分类
+    delSort: (req, res) => {
+      
+            let id = req.query.id
+            // console.log(id);
+            let delSql = `DELETE FROM categories WHERE id=${id}`
+            categoriesdb.query(delSql, (err, result) => {
+                if (err) {
+                    res.send({
+                        status: 400,
+                        msg: '出错了'
+
+                    })
+                    return
+                }
+                res.send({
+                    status: 200,
+                    msg: '删除成功'
+                })
+            })
         
-    }
+    },
+
+    //处理批量删除
+    delAllSort: (req, res) => {
+        
+            let ids = req.body
+            // console.log(ids);
+            let idStr = ids.id.join(',')
+            let delAllSql = `DELETE FROM categories WHERE id in (${idStr})`
+            // console.log(delAllSql);
+            categoriesdb.query(delAllSql, (err, result) => {
+                if (err) {
+                    res.send({
+                        status: 400,
+                        msg: '出错了'
+                    })
+                    return
+                }
+                res.send({
+                    status: 200,
+                    msg: '删除成功'
+                })
+            })
+        }
+    
 
 }
--- router/categoriesRouter.js
+++ router/categoriesRouter.js
@@ -7,5 +7,8 @@ router.get('/categories',categoriesContr.categories)//加载静态页面
         .get('/getAllData',categoriesContr.getAllData)//处理动态渲染数据路由
         .post('/addSort',categoriesContr.addSort)//处理新增分类数据
         .get('/selSortById',categoriesContr.selSortById)//处理编辑分类内容
+        .post('/updateCategories',categoriesContr.updateCategories)//处理编辑提交的数据
+        .get('/delSort',categoriesContr.delSort)//处理删除分类
+        .post('/delAllSort',categoriesContr.delAllSort)//批量删除
 //暴露接口
 module.exports = router
